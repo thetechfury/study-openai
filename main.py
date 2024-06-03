fcb0e2ec07d8b63863a95b33ad44a44fb95f342e
junaid <mjunaidiqbal@thetechfury.com>
2024-06-03 12:18:48 +0500

    delete env and extra code and add gitignore file

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-from langchain_core.prompts import PromptTemplate
 import streamlit as st
 from dotenv import load_dotenv
-from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import FAISS
-from langchain_openai import OpenAI
 from langchain_community.document_loaders import TextLoader
 import os
 import openai
- 
+
 
 def main():
     # Load environment variables
     load_dotenv()
 
     # Set OpenAI API key
-    client = openai.OpenAI(
-        # This is the default and can be omitted
-        api_key=os.environ.get("OPENAI_API_KEY"),
-    )
+    openai.api_key = os.getenv("OPENAI_API_KEY")
 
     st.header("Chatbot with Custom Data 💬")
 
     # Load and split documents
-    raw_documents = TextLoader('./short-stories.txt', encoding = 'UTF-8').load()
+    raw_documents = TextLoader('./short-stories.txt', encoding='UTF-8').load()
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
     documents = text_splitter.split_documents(raw_documents)
 
@@ -50,10 +44,10 @@ def main():
 
         # Embed the user query
         embedding_vector = OpenAIEmbeddings().embed_query(query)
-        
+
         # Perform similarity search on the FAISS index
         docs = db.similarity_search_by_vector(embedding_vector)
-        
+
         # Extract content from the most relevant document
         if docs:
             related_content = docs[0].page_content
@@ -76,7 +70,9 @@ def main():
         st.session_state.messages.append({'role': 'assistant', 'content': bot_response})
 
         # Display bot response
-        st.text_area("Bot:", value=bot_response, height=50, max_chars=None, key=f"response_{len(st.session_state.messages)}")
+        st.text_area("Bot:", value=bot_response, height=50, max_chars=None,
+                     key=f"response_{len(st.session_state.messages)}")
+
 
 if __name__ == "__main__":
     main()
